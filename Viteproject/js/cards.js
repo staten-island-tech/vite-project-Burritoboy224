163faa85dab7191400ad35d51e2fcd01db2e986a
Burritoboy224 <fir3panic@gmail.com>
2023-11-19 21:04:57 -0500

    qwertyuiopasdfghjklzxcvbnm

---
+++ Viteproject/js/cards.js
@@ -0,0 +1,210 @@
+const cards = [
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+
+    {
+        name: "",
+        type: "",
+        region: "",
+        image: "",
+
+    },
+]
---
+++ Viteproject/js/dom.js
@@ -0,0 +1,7 @@
+const DOMselectors = {
+    name: document.querySelector("#name-card"),
+    type: document.querySelector("#type-card"),
+    region: document.querySelector("#region-type"),
+    image: document.querySelector("#image-card"),
+}
+export {DOMselectors};
