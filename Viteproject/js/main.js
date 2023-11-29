f84e66cb7ae9651f3eb8708f8264eecc522f1e0a
Burritoboy224 <fir3panic@gmail.com>
2023-11-28 23:33:01 -0500

    asdasd

--- Viteproject/js/main.js
+++ Viteproject/js/main.js
@@ -2,13 +2,45 @@ import "../style.css";
 import {DOMselectors} from './dom'
 import { cards } from "./cards";
 
-DOMselectors.all.AddEventListener("click",function() {
-    DOMselectors.box.innerHTML = "";
-    inserts (cards);
-});
 
-//DOMselectors.form.addEventListener("click", function () {
 
+
+
+  DOMselectors.form.addEventListener("click", function () {
+    DOMselectors.menu.innerHTML = "";
+    const regions = cards.filter((regions) => regions.region.includes("Sinnoh"));
+    inserts(regions);
+  });
+
+  DOMselectors.form.addEventListener("click", function () {
+    DOMselectors.menu.innerHTML = "";
+    const regions = cards.filter((regions) => regions.region.includes("Kanto"));
+    inserts(regions);
+  });
+
+  DOMselectors.form.addEventListener("click", function () {
+    DOMselectors.menu.innerHTML = "";
+    const regions = cards.filter((regions) => regions.region.includes("Hoenn"));
+    inserts(regions);
+  });
+
+  DOMselectors.form.addEventListener("click", function () {
+    DOMselectors.menu.innerHTML = "";
+    const regions = cards.filter((regions) => regions.region.includes("Unova"));
+    inserts(regions);
+  });
+
+  DOMselectors.form.addEventListener("click", function () {
+    DOMselectors.menu.innerHTML = "";
+    const regions = cards.filter((regions) => regions.region.includes("Kalos"));
+    inserts(regions);
+  });
+
+  DOMselectors.form.addEventListener("click", function () {
+    DOMselectors.menu.innerHTML = "";
+    const includesRegions = cards.filter((regions) => regions.region.includes("Alola"));
+    inserts(regions);
+  });
 
 //change theme
 
