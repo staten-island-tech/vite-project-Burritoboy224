643d241d73c77d957b74d9c0bd56ffcec32504f8
Burritoboy224 <fir3panic@gmail.com>
2023-11-28 23:23:38 -0500

    yuuuu

--- Viteproject/js/main.js
+++ Viteproject/js/main.js
@@ -7,41 +7,34 @@ DOMselectors.all.AddEventListener("click",function() {
     inserts (cards);
 });
 
-DOMselectors.form.addEventListener("click", function () {
-    DOMselectors.menu.innerHTML = "";
-    const regions = cards.filter((regions) => regions.region.includes("Sinnoh"));
-    inserts(regions);
-  });
-
-  DOMselectors.form.addEventListener("click", function () {
-    DOMselectors.menu.innerHTML = "";
-    const regions = cards.filter((regions) => regions.region.includes("Kanto"));
-    inserts(regions);
-  });
-
-  DOMselectors.form.addEventListener("click", function () {
-    DOMselectors.menu.innerHTML = "";
-    const regions = cards.filter((regions) => regions.region.includes("Hoenn"));
-    inserts(regions);
-  });
-
-  DOMselectors.form.addEventListener("click", function () {
-    DOMselectors.menu.innerHTML = "";
-    const regions = cards.filter((regions) => regions.region.includes("Unova"));
-    inserts(regions);
-  });
-
-  DOMselectors.form.addEventListener("click", function () {
-    DOMselectors.menu.innerHTML = "";
-    const regions = cards.filter((regions) => regions.region.includes("Kalos"));
-    inserts(regions);
-  });
-
-  DOMselectors.form.addEventListener("click", function () {
-    DOMselectors.menu.innerHTML = "";
-    const includesRegions = cards.filter((regions) => regions.region.includes("Alola"));
-    inserts(regions);
-  });
+//DOMselectors.form.addEventListener("click", function () {
 
 
+//change theme
 
+document.querySelector("#changer-button").addEventListener("click", function() {
+  if(document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  }
+  else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
+
+//display cards
+function insertCards() {
+  cards.forEach((card) => DOMselectors.menu.insertAdjacentHTML("beforeend", 
+  `<div class="card" id="all">
+  <h2 class="name">${card.name}</h2>
+  <img src="${card.image}" alt="${card.name}" class="image">
+  <h3 class="text">${card.type}</h3>
+</div>`
+)
+);
+}
+DOMselectors.All.addEventListener("click", function () {
+  DOMselectors.menu.innerHTML = ' ';
+  insertCards();
+});
